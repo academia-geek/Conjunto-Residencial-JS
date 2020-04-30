87932e1ca550417495f15a6015b6954bfacca66e
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-04-30 10:00:53 -0500

    funcion para calcular la edad!!!

--- js/clases/Propietario.js
+++ js/clases/Propietario.js
@@ -14,6 +14,18 @@ export default class Propietario{
         return this.nombre;
     }
     
+    calcularEdad(){
+        var hoy = new Date();
+        var cumpleanos = new Date(this.fecha_nacimiento);
+        var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+        var m = hoy.getMonth() - cumpleanos.getMonth();
+    
+        if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+            edad--;
+        }
+        return edad;        
+    }
+    
 }
 
 //export default Propietario
